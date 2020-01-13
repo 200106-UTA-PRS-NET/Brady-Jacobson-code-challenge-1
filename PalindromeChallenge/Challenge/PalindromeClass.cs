43ad473f23cfd30204ab516ed09fc0ccd2655f7d
BradyJacobson <u0993342@utah.edu>
2020-01-13 13:54:51 -0600

    Updated code to pass 8/8 tests.

--- PalindromeChallenge/Challenge/PalindromeClass.cs
+++ PalindromeChallenge/Challenge/PalindromeClass.cs
@@ -6,37 +6,54 @@ namespace Challenge
     {
         public bool checkPalindrome(string original)
         {
+            //Sets up the string so that capital letters don't cause issues.
             string item = original.ToUpper();
             bool result = false;
+
+            //Sets up which index of the string is checked at each comparison.
             int index1;
             int index2;
             index1 = 0;
             index2 = item.Length-1;
-            char aa = 'a';
-            
+
+            //The for loop that's guaranteed to run through all characters of the string.
             for (int i = 0; i < item.Length; i++)
             {
+                //If index1 passes index2, it shows the provided string is a palindrome.
+                //We break out of the for loop and return true.
                 if(index1 > index2)
                 {
                     result = true;
                     break;
                 }
 
+                //If index1's value equals index2's value, the values are equal, and it's still
+                //possibly a palindrome. We increment index1 and decrement index2.
                 if (item[index1] == item[index2])
                 {
                     index1++;
                     index2--;
                 }
+
+                //If they are not equal, we check to see if any of the values are spaces or punctuation.
                 else
                 {
-                    if (item[index1] == ' ' || item[index2] == ' ')
-                    {
-                        if (item[index1] == ' ')
-                            index1++;
-                        if (item[index2] == ' ')
-                            index2--;
+                    //If the value of an index is not a letter or number, we increment or decrement the
+                    //index and set the flag to true. 
+                    bool checkFlag = false;
+                    if (((int)item[index1] < 65 || (int)item[index1] > 90) && ((int)item[index1] < 48 || (int)item[index1] > 57))
+                    { 
+                        checkFlag = true;
+                        index1++;
                     }
-                    else
+                    if (((int)item[index2] < 65 || (int)item[index2] > 90) && ((int)item[index2] < 48 || (int)item[index2] > 57))
+                    {
+                        checkFlag = true;
+                        index2--;
+                    }
+                    //If the flag is false, the compared values are not equal and are either letters or numbers.
+                    //That means it is not a palindrome and we return false after breaking.
+                    if (checkFlag == false)
                     {
                         result = false;
                         break;
--- PalindromeChallenge/challengeTest1/UnitTest1.cs
+++ PalindromeChallenge/challengeTest1/UnitTest1.cs
@@ -7,46 +7,67 @@ namespace challengeTest1
     public class UnitTest1
     {
         PalindromeClass p = new PalindromeClass();
+        //Tests a simple palindrome with a space.
         [TestMethod]
         public void TestMethod1()
         {
             bool r = p.checkPalindrome("nurses run");
             Assert.IsTrue(r);
         }
+
+        //Tests a simple palindrome with capitals.
         [TestMethod]
         public void TestMethod2()
         {
             bool r = p.checkPalindrome("racecaR");
             Assert.IsTrue(r);
         }
+        
+        //Tests a simple palindrome with numbers
         [TestMethod]
         public void TestMethod3()
         {
             bool r = p.checkPalindrome("1221");
             Assert.IsTrue(r);
         }
+
+        //Tests a simple palindrome with commas
         [TestMethod]
         public void TestMethod4()
         {
             bool r = p.checkPalindrome("never odd, or even");
             Assert.IsTrue(r);
         }
+
+        //Tests a failiure of a palindrome with spelt numbers.
         [TestMethod]
         public void TestMethod5()
         {
             bool r = p.checkPalindrome("one two one");
             Assert.IsFalse(r);
         }
+
+        //Tests a failiure of a palindrome with letters and numbers
         [TestMethod]
         public void TestMethod6()
         {
             bool r = p.checkPalindrome("123abccba123");
             Assert.IsFalse(r);
         }
+
+        //Tests a simple palindrome with random spaces
         [TestMethod]
         public void TestMethod7()
         {
             bool r = p.checkPalindrome("so i said i as i os");
+            Assert.IsTrue(r);
+        }
+
+        //Tests a simple palindrome with a variety of values that don't count.
+        [TestMethod]
+        public void TestMethod8()
+        {
+            bool r = p.checkPalindrome("0#95a.a,59,0,");
             Assert.IsTrue(r);
         }
     }
